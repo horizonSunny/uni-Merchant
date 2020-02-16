17bfd3a3902b5e8851a2914b002cabb8ca074ca4
孙裕 <sunyu@sunyudeMacBook-Pro.local>
2020-02-16 17:29:47 +0800

    login sms over

--- src/store/index.js
+++ src/store/index.js
@@ -1,27 +1,27 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 const store = new Vuex.Store({
-	state: {
-		/**
-		 * 是否需要强制登录
-		 */
-		forcedLogin: false,
-		hasLogin: false,
-		userName: ""
-	},
-	mutations: {
-		login(state, userName) {
-			state.userName = userName || '新用户';
-			state.hasLogin = true;
-		},
-		logout(state) {
-			state.userName = "";
-			state.hasLogin = false;
-		}
-	}
-})
+  state: {
+    /**
+     * 是否需要强制登录
+     */
+    forcedLogin: false,
+    hasLogin: true,
+    userName: ""
+  },
+  mutations: {
+    login(state, userName) {
+      state.userName = userName || "新用户";
+      state.hasLogin = true;
+    },
+    logout(state) {
+      state.userName = "";
+      state.hasLogin = false;
+    }
+  }
+});
 
-export default store
+export default store;
