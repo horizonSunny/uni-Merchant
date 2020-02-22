db956025992cf6a8e678b1aaf1dd34999141f5c3
孙裕 <sunyu@sunyudeMacBook-Pro.local>
2020-02-22 11:40:00 +0800

    add

--- src/pages/classify/classify.vue
+++ src/pages/classify/classify.vue
@@ -2,17 +2,19 @@
   <view class="content">
     <view class="siderBar">
       <scroll-view class="scroll-view" scroll-y>
-        <view style="height:300px;background:yellow">1级分了</view>
-        <view style="height:300px;background:yellow">1级分了</view>
-        <view style="height:300px;background:yellow">1级分了</view>
-        <view style="height:300px;background:yellow">1级分了</view>
-        <view style="height:300px;background:yellow">1级分了</view>
-        <view style="height:300px;background:yellow">1级分了</view>
+        <view
+          class="firstClassify"
+          v-for="(item, index) in classify"
+          :class="{ active: index === tabCurrentIndex }"
+          :key="index"
+          @click="selectBar(index)"
+          >{{ item.cateName }}</view
+        >
       </scroll-view>
     </view>
     <view class="classifyContent">
       <scroll-view class="scroll-view" scroll-y>
-        <view style="height:200px;background:green">1级分了</view>
+        <view style="height:200px;background:green">1级分类</view>
         <view style="height:200px;background:green">1级分了</view>
         <view style="height:200px;background:green">1级分了</view>
         <view style="height:200px;background:green">1级分了</view>
@@ -22,35 +24,35 @@
   </view>
 </template>
 <script>
-import { mapActions } from "vuex";
+import { mapActions, mapGetters } from "vuex";
 export default {
   components: {
     // menuModal
   },
   data() {
     return {
-      categoryList: [],
-      menuList: []
+      tabCurrentIndex: 0
     };
   },
   onLoad() {
     // this.getClassify();
     console.log("this.$store_", this.$store);
-
-    this.$store.dispatch("GetClassify").then(() => {
-      console.log(
-        "this.$store.getters.classify_",
-        this.$store.getters.classify
-      );
+    this.getClassify().then(() => {
+      console.log("this.$store.getters");
     });
   },
-
+  computed: {
+    ...mapGetters(["classify"])
+  },
   methods: {
     ...mapActions({
       getClassify: "GetClassify" // 将 `this.add()` 映射为 `this.$store.dispatch('increment')`
     }),
     change(e) {
       this.current = e.detail.current;
+    },
+    selectBar(index) {
+      this.tabCurrentIndex = index;
     }
   }
 };
@@ -67,16 +69,30 @@ uni-page-body {
   width: 100%;
   background: #fff;
   .siderBar {
-    width: 20%;
-    .scroll-view {
-      height: 100%;
+    width: 25%;
+    text-align: center;
+    .firstClassify {
+      margin-top: 26px;
+      display: inline-block;
+      text-align: center;
+      width: 85%;
+      border-radius: 13px;
+      height: 26px;
+      line-height: 26px;
+      font-size: 14px;
+      font-family: PingFangSC-Regular, PingFang SC;
+      font-weight: 400;
+    }
+    .active {
+      background: #3a74f1;
+      color: #fff;
     }
   }
   .classifyContent {
     flex: 1;
-    .scroll-view {
-      height: 100%;
-    }
+  }
+  .scroll-view {
+    height: 100%;
   }
 }
 </style>
