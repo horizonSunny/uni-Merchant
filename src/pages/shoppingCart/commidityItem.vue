7e82b05eef71b4ed63dd71b83a1b597e67d7048e
sunyu1 <sunyu1@green-valley.com>
2020-03-05 19:38:14 +0800

    add

--- src/pages/shoppingCart/commidityItem.vue
+++ src/pages/shoppingCart/commidityItem.vue
@@ -1,52 +1,64 @@
 <template>
-  <checkbox-group @change="checkboxChange">
-    <label
-      class="uni-list-cell uni-list-cell-pd"
-      v-for="item in items"
-      :key="item.value"
-    >
-      <uni-swipe-action>
-        <uni-swipe-action-item
-          :options="options"
-          @click="onClick"
-          @change="change"
-          @click.stop
-        >
-          <view class="commidityInfo" @click.stop>
-            <checkbox
-              :value="item.value"
-              :checked="item.checked"
-              color="#FFCC33"
-            />
-            <view class="productImg">
-              <img src="/static/img/home.png" alt="" width="60" height="60" />
-              <view class="model">已下架</view>
-            </view>
-            <view class="drugsInfo">
-              <view class="drugName">
-                <text class="mark">OTC</text>
-                <!-- <text class="mark" v-show="item.isMp === 0">OTC</text>
+  <view>
+    <checkbox-group @change="checkboxChange">
+      <label
+        class="uni-list-cell uni-list-cell-pd"
+        v-for="item in items"
+        :key="item.value"
+      >
+        <uni-swipe-action>
+          <uni-swipe-action-item
+            :options="options"
+            @click="onClick"
+            @change="change"
+            @click.stop
+          >
+            <view class="commidityInfo" @click.stop>
+              <checkbox
+                :value="item.value"
+                color="#FFCC33"
+                :checked="checkedArr.includes(String(item.value))"
+              />
+              <view class="productImg">
+                <img src="/static/img/home.png" alt="" width="60" height="60" />
+                <view class="model">已下架</view>
+              </view>
+              <view class="drugsInfo">
+                <view class="drugName">
+                  <text class="mark">OTC</text>
+                  <!-- <text class="mark" v-show="item.isMp === 0">OTC</text>
                 <text class="mark" v-show="item.isMp === 1">双规</text>
                 <text class="mark" v-show="item.isMp === 2">RX</text>
                 <text class="mark" v-show="item.isMp === 3">其他</text> -->
-                <text>爱康国宾 疾病 宾 疾病</text>
-                <view class="drugSpec">乳腺癌检测 1次</view>
-                <!-- <text>{{ item.productName }}</text> -->
-              </view>
-              <!-- <view class="drugSpec">{{ item.productSpecif }}</view> -->
-              <view class="drugPrice">
-                <text>
-                  ¥ 123
-                </text>
-                <yp-number-box :min="0" :max="9"></yp-number-box>
-                <!-- {{ item.price }} -->
+                  <text>爱康国宾 疾病 宾 疾病</text>
+                  <view class="drugSpec">乳腺癌检测 1次</view>
+                  <!-- <text>{{ item.productName }}</text> -->
+                </view>
+                <!-- <view class="drugSpec">{{ item.productSpecif }}</view> -->
+                <view class="drugPrice">
+                  <text>
+                    ¥ 123
+                  </text>
+                  <yp-number-box :min="0" :max="9"></yp-number-box>
+                  <!-- {{ item.price }} -->
+                </view>
               </view>
             </view>
-          </view>
-        </uni-swipe-action-item>
-      </uni-swipe-action>
-    </label>
-  </checkbox-group>
+          </uni-swipe-action-item>
+        </uni-swipe-action>
+      </label>
+    </checkbox-group>
+    <checkbox-group @change="allChoose">
+      <label>
+        <checkbox
+          value="all"
+          :class="{ checked: allChecked }"
+          :checked="allChecked ? true : false"
+        ></checkbox>
+        全选
+      </label>
+    </checkbox-group>
+  </view>
 </template>
 <script>
 import uniSwipeAction from '@/components/uni-swipe-action/uni-swipe-action.vue'
@@ -98,20 +110,49 @@ export default {
             fontSize: '15px'
           }
         }
-      ]
+      ],
+      checkedArr: [], //复选框选中的值
+      allChecked: false //是否全选
     }
   },
   methods: {
     checkboxChange: function (e) {
-      var items = this.items,
-        values = e.detail.value;
-      for (var i = 0, lenI = items.length; i < lenI; ++i) {
-        const item = items[i]
-        if (values.includes(item.value)) {
-          this.$set(item, 'checked', true)
-        } else {
-          this.$set(item, 'checked', false)
+      this.checkedArr = e.detail.value;
+      // 如果选择的数组中有值，并且长度等于列表的长度，就是全选
+      if (this.checkedArr.length > 0 && this.checkedArr.length == this.items.length) {
+        this.allChecked = true;
+      } else {
+        this.allChecked = false;
+      }
+      // var items = this.items,
+      //   values = e.detail.value;
+      // for (var i = 0, lenI = items.length; i < lenI; ++i) {
+      //   const item = items[i]
+      //   if (values.includes(item.value)) {
+      //     this.$set(item, 'checked', true)
+      //   } else {
+      //     this.$set(item, 'checked', false)
+      //   }
+      // }
+    },
+    // 全选事件
+    allChoose (e) {
+      let chooseItem = e.detail.value;
+      // 全选
+      if (chooseItem[0] == 'all') {
+        this.allChecked = true;
+        for (let item of this.items) {
+          let itemVal = String(item.value);
+          if (!this.checkedArr.includes(itemVal)) {
+            this.checkedArr.push(itemVal);
+          }
         }
+      } else {
+        // 取消全选
+        console.log('取消全选');
+
+        this.allChecked = false;
+        this.checkedArr = [];
       }
     },
     onClick (e) {
