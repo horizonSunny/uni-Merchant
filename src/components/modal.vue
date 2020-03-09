2b886278807eee86417636aab154cffd83f60cfd
sunyu1 <sunyu1@green-valley.com>
2020-03-09 09:16:01 +0800

    add

---
+++ src/components/modal.vue
@@ -0,0 +1,19 @@
+<template>
+  <view class="modal">
+    <slot></slot>
+  </view>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="scss" scoped>
+.modal {
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  z-index: 998;
+  background: rgba($color: #000000, $alpha: 0.7);
+}
+</style>
---
+++ src/pages/indent/purchasefailed.vue
@@ -0,0 +1,28 @@
+<template>
+  <modal>
+    <view class="purchasefailed"> </view>
+  </modal>
+</template>
+<script>
+import modal from '@/components/modal.vue';
+export default {
+  components: {
+    // commidityItem
+    modal
+  },
+}
+</script>
+<style lang="scss" scoped>
+.purchasefailed {
+  height: 380px;
+  width: 94%;
+  position: absolute;
+  left: 50%;
+  margin-left: -47%;
+  top: 50%;
+  margin-top: -190px;
+  background: #fff;
+  opacity: 1;
+  z-index: 999;
+}
+</style>
