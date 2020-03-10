8dd5014870a9fd76c94e1d4ad2529ebaa857042c
sunyu1 <sunyu1@green-valley.com>
2020-03-10 12:38:09 +0800

    add

--- src/pages/prescription/diseasesHistory.vue
+++ src/pages/prescription/diseasesHistory.vue
@@ -6,31 +6,47 @@
         <view class="close" @click="closeModal">X</view>
       </view>
       <view class="content">
-        <view class="contentDis">
-          <span>性别</span>
-          <radio-group @change="sexChange">
-            <view class="sexSel">
-              <label
-                v-for="(item, index) in items"
-                :key="item.value"
-                style="margin-right:20px;"
-              >
-                <view>
-                  <radio
-                    :value="item.value"
-                    :checked="index === currentSex"
-                  />{{ item.name }}
-                </view>
-              </label>
-            </view>
-          </radio-group>
+        <view class="contentWrap" v-for="(item, index) in test" :key="index">
+          <view class="contentDis">
+            <span>{{ item.name }}</span>
+            <radio-group @change="diseaChange($event, item)">
+              <view class="choose">
+                <label
+                  v-for="(itemInfo, indexInfo) in items"
+                  :key="indexInfo"
+                  style="margin-right:20px;"
+                >
+                  <view>
+                    <radio
+                      :value="itemInfo.value"
+                      :checked="itemInfo.value == item.status"
+                    />{{ itemInfo.name }}
+                  </view>
+                </label>
+              </view>
+            </radio-group>
+          </view>
+          <view v-show="item.status == 1" class="diseases">
+            <view
+              class="diseasesItem"
+              v-for="(diseasesInfo, diseasesIndex) in item.diseases"
+              :key="diseasesIndex"
+              >{{ diseasesInfo }}</view
+            >
+          </view>
         </view>
+      </view>
+      <view class="confirm">
+        <button type="primary" class="save" @click="submit">
+          完成
+        </button>
       </view>
     </view>
   </modal>
 </template>
 <script>
 import modal from '@/components/modal.vue';
+import test from './test'
 export default {
   components: {
     modal
@@ -42,25 +58,26 @@ export default {
       // 性别
       currentSex: '0',
       items: [
-        { value: '0', name: "男士" },
-        { value: '1', name: "女士" }
+        { value: '1', name: "有" },
+        { value: '0', name: "无" }
       ],
+      test: test
     }
   },
   methods: {
-    radioChange: function (evt) {
-      for (let i = 0; i < this.items.length; i++) {
-        if (this.items[i].value === evt.target.value) {
-          this.current = i;
-          break;
-        }
-      }
-    },
     closeModal () {
       this.modal = false;
     },
     openModal () {
       this.modal = true;
+    },
+    // 选中有无时候
+    diseaChange (evt, item) {
+      console.log('evt_', evt.detail.value);
+      console.log('item_', item.status);
+      item.status = evt.detail.value
+      console.log('test_', test);
+
     }
   }
 }
@@ -72,8 +89,6 @@ export default {
   bottom: 0px;
   width: 100%;
   background: #fff;
-  display: flex;
-  flex-direction: column;
   .title {
     height: 40px;
     width: 100%;
@@ -101,12 +116,62 @@ export default {
   .content {
     flex: 1;
     padding: 0px 20px;
-    background: red;
-    .contentDis {
-      list-style: none;
+    height: 350px;
+    overflow: scroll;
+    .contentWrap {
       min-height: 59px;
-      width: 100%;
-      background: yellow;
+      border-top: 1px solid #efefef;
+      .contentDis {
+        list-style: none;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        .choose {
+          display: flex;
+          height: 57px;
+          line-height: 57px;
+          /deep/ .uni-radio-input {
+            position: relative;
+            top: -2px;
+          }
+        }
+      }
+      .diseases {
+        height: auto;
+        width: auto;
+        justify-content: flex-start;
+        padding-bottom: 15px;
+        .diseasesItem {
+          width: auto;
+          margin-top: 10px;
+          height: 30px;
+          line-height: 30px;
+          display: inline-block;
+          background: rgba(242, 242, 242, 1);
+          border-radius: 4px;
+          margin-right: 10px;
+          font-size: 12px;
+          padding: 0 14px;
+          font-family: PingFangSC-Regular, PingFang SC;
+          font-weight: 400;
+          color: rgba(27, 27, 27, 1);
+        }
+      }
+    }
+  }
+  .confirm {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    flex: 1;
+    margin-top: 12px;
+    button {
+      height: 47px;
+      width: 80%;
+    }
+    .save {
+      background: #3a74f1;
     }
   }
 }
--- src/pages/prescription/test.js
+++ src/pages/prescription/test.js
@@ -1,32 +1,43 @@
-export default diseasesHistory = [
+let diseasesHistory = [
   {
     name: '过往病史',
     id: '0',
-    confirm: true,
-    diseases: ['糖尿病', '恶性肿瘤', '心脏病']
+    status: 0,
+    diseases: [
+      '糖尿病',
+      '恶性肿瘤',
+      '心脏病',
+      '糖尿病',
+      '恶性肿瘤',
+      '心脏病',
+      '糖尿病',
+      '恶性肿瘤',
+      '心脏病'
+    ]
   },
   {
     name: '过敏史',
     id: '1',
-    confirm: true,
+    status: 0,
     diseases: ['过敏史病', '恶性肿瘤', '心脏病']
   },
   {
     name: '家族病史',
     id: '2',
-    confirm: true,
+    status: 0,
     diseases: ['家族病史', '恶性肿瘤', '心脏病']
   },
   {
     name: '肾功能',
     id: '3',
-    confirm: true,
+    status: 0,
     diseases: ['肾功能', '恶性肿瘤', '心脏病']
   },
   {
     name: '肝功能',
     id: '4',
-    confirm: true,
+    status: 1,
     diseases: ['肝功能', '恶性肿瘤', '心脏病']
   }
 ]
+export default diseasesHistory
