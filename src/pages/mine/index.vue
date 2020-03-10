10966db908adb74b207864255857c32e6e19a72b
sunyu1 <sunyu1@green-valley.com>
2020-03-10 17:01:44 +0800

    add

--- src/pages/mine/index.vue
+++ src/pages/mine/index.vue
@@ -4,7 +4,28 @@
       <text slot="title">个人中心</text>
     </tob-bar>
     <view slot="content" class="content">
-      <view class="head"> </view>
+      <view class="head">
+        <view class="headPortrait">
+          <img src="/static/mine/mine_default head.svg" alt="" />
+          <text>136****9087</text>
+        </view>
+        <view class="classify">
+          <view class="classifyItem">
+            <view>128</view>
+            <view>浏览记录</view>
+          </view>
+          <view class="separator"></view>
+          <view class="classifyItem">
+            <view>39</view>
+            <view>收藏</view>
+          </view>
+          <view class="separator"></view>
+          <view class="classifyItem">
+            <view>104</view>
+            <view>药店关注</view>
+          </view>
+        </view>
+      </view>
     </view>
     <tab-bar slot="tabBar"></tab-bar>
   </body-wrap>
@@ -91,7 +112,44 @@ export default {
     .head {
       width: 100%;
       height: 138px;
-      background: #477efb;
+      padding: 27px 0px 0px 0px;
+      background: url("../../static/mine/mine_background.svg");
+      font-size: 16px;
+      font-family: PingFangSC-Semibold, PingFang SC;
+      font-weight: 600;
+      color: rgba(255, 255, 255, 1);
+      .headPortrait {
+        height: 44px;
+        display: flex;
+        margin-left: 14px;
+        align-items: center;
+        img {
+          height: 44px;
+          width: 44px;
+        }
+        text {
+          height: 22px;
+          line-height: 22px;
+          margin-left: 10px;
+        }
+      }
+      .classify {
+        margin-top: 25px;
+        height: 40px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        .classifyItem {
+          width: 97px;
+          height: 40px;
+          text-align: center;
+        }
+        .separator {
+          height: 12px;
+          background: rgba(255, 255, 255, 1);
+          width: 2px;
+        }
+      }
     }
   }
 }
