c675d45ef1b83a72e4576ed3dd075ca3106c51f5
sunyu1 <sunyu1@green-valley.com>
2020-03-11 13:54:00 +0800

    add

---
+++ src/config/json.js
@@ -0,0 +1,38 @@
+const tabList = [
+  {
+    name: '默认',
+    id: '1'
+  },
+  {
+    name: '销量',
+    id: '2'
+  },
+  {
+    name: '价格',
+    id: '3'
+  }
+]
+
+const indentTabList = [
+  {
+    name: '全部',
+    id: '1'
+  },
+  {
+    name: '待付款/审核',
+    id: '2'
+  },
+  {
+    name: '待发货',
+    id: '3'
+  },
+  {
+    name: '待收货',
+    id: '4'
+  },
+  {
+    name: '待评价',
+    id: '5'
+  }
+]
+export { tabList, indentTabList }
