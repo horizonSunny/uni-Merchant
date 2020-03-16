04d555b58f101612216fe599d5ef708f8b4317d6
sunyu1 <sunyu1@green-valley.com>
2020-03-16 16:07:12 +0800

    add

--- src/pages/commodityDetails/index.vue
+++ src/pages/commodityDetails/index.vue
@@ -23,8 +23,32 @@
           >{{ item }}</text
         >
       </view>
-      <img src="static/icon/commodityDetails/more.svg" alt class="more" />
-      <img src="static/icon/commodityDetails/shopCar.svg" alt class="shopCar" />
+      <img
+        src="static/icon/commodityDetails/more.svg"
+        alt
+        class="more"
+        @click="showPage"
+      />
+      <img
+        src="static/icon/commodityDetails/shopCar.svg"
+        alt
+        class="shopCar"
+        @click="goPage('../shoppingCart/index')"
+      />
+    </view>
+    <view class="jumpPage" v-show="jumpPageShow">
+      <view class="angle"></view>
+      <view
+        class="pageItem"
+        v-for="(item, index) in page"
+        :key="index"
+        @click="goPage(item.url)"
+      >
+        <img :src="item.img" alt="" />
+        <view>
+          {{ item.name }}
+        </view>
+      </view>
     </view>
     <!-- bottomBar -->
     <view class="bottomBar">
@@ -332,6 +356,14 @@ export default {
       uni.navigateTo({
         url: "../login/login"
       });
+    },
+    // more　
+    showPage () {
+      this.jumpPageShow = !this.jumpPageShow
+    },
+    goPage (url) {
+      console.log(url);
+      this.$navTo(url);
     }
   },
   data () {
@@ -352,7 +384,32 @@ export default {
       swiperslcInfo: 0,
       basicInfo: [
 
-      ]
+      ],
+      jumpPageShow: false,
+      page: [{
+        url: '../main/main',
+        name: '首页',
+        img: '/static/icon/main/D_home.svg'
+      }, {
+        url: '../shoppingCart/index',
+        name: '购物车',
+        img: '/static/icon/main/D_Shopping Cart.svg'
+      },
+      {
+        url: '../search/search',
+        name: '搜索',
+        img: '/static/icon/main/D_search.svg'
+      },
+      {
+        url: '',
+        name: '消息',
+        img: '/static/icon/main/D_news.svg'
+      },
+      {
+        url: '../mine/index',
+        name: '我的',
+        img: '/static/icon/main/D_my.svg'
+      }],
     };
   }
 };
@@ -761,6 +818,50 @@ uni-page-body {
   uni-swiper {
     display: block;
     height: 200px;
+  }
+  .jumpPage {
+    z-index: 9999;
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    right: 5px;
+    top: 38px;
+    width: 130px;
+    height: 214px;
+    background: rgba(255, 255, 255, 1);
+    box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.15);
+    border-radius: 6px;
+    .angle {
+      width: 0;
+      height: 0;
+      position: absolute;
+      top: -18px;
+      right: 20px;
+      border-width: 10px;
+      border-style: solid;
+      border-color: transparent #fff transparent transparent;
+      background: rgba(255, 255, 255, 0);
+      transform: rotate(90deg); /*顺时针旋转90°*/
+    }
+    .pageItem {
+      background: #fff;
+      // padding-left: 50px;
+      height: 42px;
+      border-bottom: 1px solid #f2f2f2;
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+      font-size: 13px;
+      font-family: PingFang-SC-Medium, PingFang-SC;
+      font-weight: 500;
+      color: rgba(27, 27, 27, 1);
+      img {
+        height: 18px;
+        width: 18px;
+        margin-left: 20px;
+        margin-right: 9px;
+      }
+    }
   }
 }
 </style>
