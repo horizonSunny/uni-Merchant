caf8d5d567e7447b9fc590f7e0f38f82dee51541
sunyu1 <sunyu1@green-valley.com>
2020-03-18 13:20:34 +0800

    add

---
+++ src/service/productVisit.js
@@ -0,0 +1,29 @@
+import http from '@/config/axios'
+import qs from 'qs'
+// 获取历史浏览信息
+async function productVisit(params) {
+  return http.get('/patient/productVisit')
+}
+// 保存商品浏览记录
+async function setProductVisit(params) {
+  return http.post('/patient/productVisit', params)
+}
+// 删除历史浏览信息
+async function deleteProductVisit(params) {
+  return http.delete('/patient/productVisit', {
+    params,
+    paramsSerializer: params => {
+      return qs.stringify(params, { indices: false })
+    }
+  })
+}
+// 清空历史浏览信息
+async function deleteProductVisitAll(params) {
+  return http.delete('/patient/productVisit/deleteAll')
+}
+export {
+  productVisit,
+  setProductVisit,
+  deleteProductVisit,
+  deleteProductVisitAll
+}
