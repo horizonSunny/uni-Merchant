e0b285fd1294f6a8521538aa43bd3350ddf23cf9
sunyu1 <sunyu1@green-valley.com>
2020-03-20 14:51:27 +0800

    add

---
+++ src/service/orderList.js
@@ -0,0 +1,32 @@
+import http from '@/config/axios'
+import qs from 'qs'
+// 获取订单列表信息
+async function getOrderList(params) {
+  return http.get('/order/order/getOrderList', { params })
+}
+// 获取支付宝接口支付
+async function alipay(params) {
+  return http.get('/order/v1/pay', { params })
+}
+// // 取消订单
+// async function cancelOrder(params) {
+//   return http.delete('/order/shopCart/delete', {
+//     data: params,
+//     paramsSerializer: params => {
+//       return qs.stringify(params, { indices: false })
+//     }
+//   })
+// }
+// 取消订单
+async function cancelOrder(params) {
+  return http.put('/order/order/cancel', params)
+}
+// 确认订单信息
+async function confirmOrder(params) {
+  return http.post('/order/order/confirmOrder', params)
+}
+// 生成订单信息
+async function generateOrder(params) {
+  return http.post('/order/order/generateOrder', params)
+}
+export { getOrderList, alipay, cancelOrder, confirmOrder, generateOrder }
