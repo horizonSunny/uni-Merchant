e7518dc154fdfcd9b17fa3d8df95a28691164c8b
sunyu1 <sunyu1@green-valley.com>
2020-06-03 11:57:46 +0800

    add

--- src/config/global.js
+++ src/config/global.js
@@ -1,7 +1,7 @@
 // const baseUrl =
 //   'https://result.eolinker.com/2IytgUe07a451eb4278c3012ed0d713642f512d34ab33ce?uri='
 // const baseUrl = 'http://47.103.158.133:8088'
-const baseUrl = 'http://192.168.5.101:9088/'
+const baseUrl = 'http://192.168.5.101:9088'
 // const baseUrl = 'http://192.168.10.234:9000'
 const channelNo = 'Basic cGF0aWVudDpwYXRpZW50'
 
